ab5e8741304edd8b30d791b411ce91b712593ef4
ErenayDev <erenaydev@proton.me>
2026-02-06 15:55:34 +0300

    fix: add signal icon

--- src/lib/components/layout/Footer/Footer.svelte
+++ src/lib/components/layout/Footer/Footer.svelte
@@ -1,13 +1,8 @@
 <script>
 	import Copyright from 'svelte-copyright';
-	import {
-		IconBrandGithub,
-		IconMail,
-		IconBrandInstagram,
-		IconBrandX,
-		IconBrandMastodon
-	} from '@tabler/icons-svelte';
+	import { IconBrandGithub, IconMail, IconBrandX, IconBrandMastodon } from '@tabler/icons-svelte';
 	import { version } from '$app/environment';
+	import SignalIcon from '$lib/components/common/SignalIcon.svelte';
 
 	const socialLinks = [
 		{
@@ -23,9 +18,9 @@
 			external: true
 		},
 		{
-			href: 'https://instagram.com/erenaydev',
-			icon: IconBrandInstagram,
-			label: 'Instagram',
+			href: 'https://signal.me/#eu/WVEzZhk76QTwwvGgw3bcXtXGwkfp8tTn-WAPuY4-L4uqJnlF98tAKaSFRKWWcHnj',
+			icon: SignalIcon,
+			label: 'Signal',
 			external: true
 		},
 		{
@@ -51,29 +46,20 @@
 		>
 			<span class="flex items-center justify-center gap-1">
 				Built with
-				<a
-					href="https://kit.svelte.dev/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="text-blue-600 transition-colors hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-				>
-					Svelte(Kit)
+				<a href="https://kit.svelte.dev/" target="_blank" rel="noopener noreferrer" class="linkify">
+					Svelte
 				</a>
 			</span>
 			<span class="flex items-center justify-center gap-1">Coded with ❤️</span>
 			<span class="flex items-center justify-center gap-1">
 				In
-				<a
-					href="https://zed.dev/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="text-blue-600 transition-colors hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-				>
+				<a href="https://zed.dev/" target="_blank" rel="noopener noreferrer" class="linkify">
 					Zed editor
 				</a>
 			</span>
 			<span class="flex items-center justify-center gap-1">In Türkiye 🇹🇷</span>
 		</div>
+
 		<div
 			class="mt-6 flex flex-col items-center justify-center space-y-4 border-t border-gray-100 pt-4 dark:border-gray-800"
 		>
@@ -85,12 +71,24 @@
 						class="flex h-9 w-9 items-center justify-center rounded-full text-slate-950 transition-all duration-300 ease-in-out hover:bg-black hover:text-white dark:text-slate-300 dark:hover:bg-white dark:hover:text-black"
 						aria-label={link.label}
 					>
-						<svelte:component this={link.icon} size={20} data-cursor={false} />
+						{#if typeof link.icon === 'string'}
+							<img src={link.icon} alt={link.label} class="h-5 w-5" />
+						{:else}
+							<svelte:component this={link.icon} size={20} data-cursor={false} />
+						{/if}
 					</a>
 				{/each}
 			</div>
+
 			<Copyright date={new Date()}>Erenay</Copyright>
 			<span class="text-xs text-gray-500 dark:text-gray-400">All rights reserved. v{version}</span>
+			<span class="text-xs text-gray-500 dark:text-gray-400"
+				>Website source code is available at <a
+					target="_blank"
+					href="https://github.com/ErenayDev/ErenayDev.com.tr"
+					class="linkify">Github repo</a
+				></span
+			>
 		</div>
 	</div>
 </footer>
